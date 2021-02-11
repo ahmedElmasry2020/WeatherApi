58f193549f23b3d4344297ae530eda8f4f7f6fec
Ahmed Elmasry <ahmed.saeed.elmasry@gmail.com>
2021-02-11 20:16:12 +0200

    Revert "Revert "change the response to changed""
    This reverts commit 908737a726b85d08131f913d1dd9727712cbb3f1.

--- routes/weather.js
+++ routes/weather.js
@@ -9,7 +9,7 @@ weather.get('/', (req, res, next) => {
         console.log(data)
         res.status(200).json({
             message: "success",
-            weather:data
+            weather:'changed'
         })
     })
     .catch(err=>{

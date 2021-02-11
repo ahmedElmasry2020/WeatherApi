541f96995c1291b90b45581c2f5f19587454246d
Ahmed Elmasry <ahmed.saeed.elmasry@gmail.com>
2021-02-11 19:46:21 +0200

    removed weather API some comments

--- dependencies/getWeatherData.js
+++ dependencies/getWeatherData.js
@@ -1,19 +1,18 @@
 const latLongDta = require('./getLongLatInfo');
 const request = require('request');
-const chalk = require('chalk');
+
 const language = require('../counteries');
 const requirePromise = require('request-promise');
 
 const getWeatherdata = async function () {
     let response = [];
-    // const 
+   
     const dataGet = await latLongDta();
-    //console.log(chalk.blue(JSON.stringify(dataGet)));
+
     const secretkey = '96e15f22ac2422205eac74849a06e137';
     const units = 'auto';
     const urlApi = `https://api.darksky.net/forecast/${secretkey}/${dataGet.lat},${dataGet.long}?units=${units}&lang=${language[0].Arabic}`;
-    // const locationInfo=dataGet;
- //    console.log(urlApi);
+  
     response.push(dataGet);
     const resp = await requirePromise({ url: urlApi, json: true });
     const weatherForcastCurrent = {
@@ -62,16 +61,10 @@ const getWeatherdata = async function () {
     }
     response.push(weatherForcastCurrent);
     response.push(weatherForcastdays);
-    //console.log(chalk.blue(JSON.stringify(resp)));
-    // const str =await resp.daily.data[0].summary + " " + "current tempature is " + resp.currently.temperature + " and probality of rain " + resp.currently.precipProbability + "%"
     return response;
-    //console.log(reverse(resp.body.daily.data[0].summary) + " " + "current tempature is " + resp.body.currently.temperature + " and probality of rain " + resp.body.currently.precipProbability + "%");
 
 
 }
 
-function reverse(s) {
-    return s.split("").reverse().join("");
-}
 
 module.exports = getWeatherdata;
